ebbdfa48285f054fd9d12f8d204c88f546c8f152
miservilla <miservilla@gmail.com>
2016-09-11 14:19:37 -0600

    Added computer as second player option.

--- src/Pig.java
+++ src/Pig.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class Pig {
 
     /**
-     * The Pig game for two players.
+     * The Pig game for two players, or against the computer playing as Pig player 2.
      * @param args Command-Line arguments are ignored.
      */
 
@@ -18,33 +18,52 @@ public class Pig {
         int playerCounter = 0;
         int turnTotal;
 
+        System.out.println("Would you like the computer to be 'Pig player 2'?");
+        String computerPigPlayer2 = YesNo();
+
         while (pigPlayer1 < 100 && pigPlayer2 < 100) {
             System.out.println("\nScore: " + pigPlayer1 + "-" + pigPlayer2 + ".");
             playerCounter++;
 
-            if (playerCounter % 2 == 0) {
-                System.out.println("\nPig player 2's turn.");
-                turnTotal = PlayerRoll();
-                pigPlayer2 += turnTotal;
+            if (computerPigPlayer2.equalsIgnoreCase("N")) {
+                if ((playerCounter % 2 == 0)){
+                    System.out.println("\nPig player 2's turn.");
+                    turnTotal = PlayerRoll();
+                    pigPlayer2 += turnTotal;
+                } else {
+                    System.out.println("\nPig player 1's turn.");
+                    turnTotal = PlayerRoll();
+                    pigPlayer1 += turnTotal;
+                }
             } else {
-                System.out.println("\nPig player 1's turn.");
-                turnTotal = PlayerRoll();
-                pigPlayer1 += turnTotal;
+                    if (playerCounter % 2 != 0) {
+                        System.out.println("\nPig player 1's turn.");
+                        turnTotal = PlayerRoll();
+                        pigPlayer1 += turnTotal;
+                    } else {
+                        System.out.println("\nComputer's turn.");
+                        turnTotal = ComputerRoll();
+                        pigPlayer2 += turnTotal;
+                    }
             }
         }
         if (pigPlayer1 > pigPlayer2){
             System.out.println("Congratulations Pig player 1, you won!");
         }
         else {
-            System.out.println("Congratulations Pig player 2, you won!");
+            if (computerPigPlayer2.equalsIgnoreCase("Y")) {
+                System.out.printf("The computer won!");
+            }else {
+                System.out.println("Congratulations Pig player 2, you won!");
+            }
         }
-
     }
+
     //Method to roll the dice.
     private static int PlayerRoll() {
-        String playAgain = "Y";
+        String playAgain;
         int turnTotal = 0;
-        while (playAgain.equalsIgnoreCase("Y")) {
+        do {
             int rollAmount = RandomNumber();
             System.out.println("You rolled a " + rollAmount + ".");
             if (rollAmount == 1) {
@@ -54,17 +73,20 @@ public class Pig {
             } else turnTotal += rollAmount;
             System.out.println("Your total this turn is " + turnTotal + ".");
             System.out.println("Roll again (y/n): ");
-            playAgain = PlayAgain();
+            playAgain = YesNo();
         }
+        while (playAgain.equalsIgnoreCase("Y"));
         return turnTotal;
     }
+
     //Method to generate random number.
-    private static int RandomNumber () {
+    private static int RandomNumber() {
         int rand = (int) (Math.random() * 6) + 1;
         return rand;
     }
+
     //Method for input error control (needs to be either y/n).
-    private static String PlayAgain() {
+    private static String YesNo() {
         Scanner scanIn = new Scanner(System.in);
         String playAgain = scanIn.next();
         while ((!playAgain.equalsIgnoreCase("Y")) && (!playAgain.equalsIgnoreCase("N"))) {
@@ -72,5 +94,24 @@ public class Pig {
             playAgain = scanIn.next();
         }
         return playAgain;
+    }
+
+    //Method for random number of times computer dice roll.
+    private static int ComputerRoll() {
+        int turnTotal = 0;
+        double computerPlayAgain;
+        do {
+            computerPlayAgain = Math.random();
+            int rollAmount = RandomNumber();
+            System.out.println("Computer rolled a " + rollAmount + ".");
+            if (rollAmount == 1) {
+                turnTotal = 0;
+                System.out.println("Computer's total this turn is " + turnTotal + ".");
+                return turnTotal;
+            } else turnTotal += rollAmount;
+            System.out.println("Computer's total this turn is " + turnTotal + ".");
+        }
+        while (computerPlayAgain < 0.8);
+        return turnTotal;
     }
 }
